5bc1a3229cfe2444fe0ea143fe661dbf8c6334ba
marvin ma <marvin.ma@redcreation.net>
2019-10-30 16:11:24 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-This is a emcas plugin to convert px unit to vw or vh in css file.
+## Introduction
+This is a emcas plugin to convert px unit to vw or vh in css or other files.
+
+## How to use
+
+* Download the `px-to-vw-vh.el` file to your emcas elisp configuration directory, generally it is `~/.emcas.d/lisp/` in MacOS. Or copy the content of `px-to-vw-vh.el` to a `.el` file in emcas el config path, and i suggest to also named `px-to-vw-vh.el`.
+
+* Add blow code to your emcas config file, `~/.emcas.d/init.el` or `~/.emcas.d/custom.el` are ok.
+```
+(require 'px-to-vw-vh)
+(global-set-key (kbd "C-c w") 'px->vw)
+```
+Now，you added a shortcut key to emacs, if it is inconvenient, you can set `F9` to do this.
+```
+(global-set-key [f9] 'px->vw)
+```
+
+* Move point to width number with "px" unit, and press shortcut key `C-c w`, px unit will convert to vw. For example:
+```
+.div {
+    width: 200px;
+}
+```
+use `C-c w` shortcur key in "200px" char, it will auto convert to
+```
+.div {
+    width: 13.89vw;
+}
+```
+
+## Explaintin
+
+The vw value is convert by expression (/ (* px 100) viewport-width) and retain two float point.
+
+## Remind
+
+I gived default width of viewport is 1440, and not support to change by param, so if you want to change, please modify the first line constant of `px-to-vw-vh.el`.
+
+
--- px-to-vw-vh.el
+++ px-to-vw-vh.el
@@ -1,4 +1,6 @@
 
+(defvar viewpoint-width 1440) 
+
 (defun kill-thing-at-point (thing)
   "Kill the `thing-at-point' for the specified kind of THING."
   (let ((bounds (bounds-of-thing-at-point thing)))
@@ -12,24 +14,26 @@
   (kill-thing-at-point 'word))
 
 (defun convert-to-vw (px-v)
-  (format "%.2f" (float (/ (* (string-to-number px-v) 100) 1440))))
+  (format "%.2f" (/ (float (* (string-to-number px-v) 100)) viewpoint-width)))
 
 (defun convert-to-vh (px-v)
-  (format "%.2f" (float (/ (* (string-to-number px-v) 100) 900))))
+  (format "%.2f" (/ (float (* (string-to-number px-v) 100)) 900 )))
 
 (defun px->vw ()
-  "Convert word at point (or selected region) to vw."
+  "Convert word at point (or selected region) from px to vw."
   (interactive)
   (let* ((char (if (use-region-p)
                      (cons (region-beginning) (region-end))
                    (thing-at-point 'symbol))))
-    (if (and (not (string-blank-p char)) (string-match "px" char))
+    (when (and char (not (string-blank-p char)) )
+      (if (string-match "px" char)
       (progn
         (setq word (current-word char))
         (setq px-v (substring word 0 (- (length word) 2)))
         (kill-word-at-point)
-        (insert (concat (convert-to-vw px-v) unit)))
-      (print "there's no px value to converting"))))
+        (insert (concat (convert-to-vw px-v) "vw")))
+      (print "there's no px value to converting")))))
 
 ;;(global-set-key (kbd "C-c w") 'px->vw)
+;;(global-set-key [f9] 'px->vw)
 (provide 'px-to-vw-vh)

49c3c336c1c4cfd73a0aea23a61ad2968dc94adb
marvin ma <marvin.ma@redcreation.net>
2019-10-31 13:50:43 +0800

    mergy

---
+++ px2vw-old.el
@@ -0,0 +1,44 @@
+;;; px2vw.el --- convert px to vw in css
+;;; Commentary:
+;;; Code:
+
+(defvar viewpoint-width 1440) 
+(defvar viewpoint-height 900) 
+
+(defun kill-thing-at-point (thing)
+  "Kill the `thing-at-point' for the specified kind of THING."
+  (let ((bounds (bounds-of-thing-at-point thing)))
+    (if bounds
+        (kill-region (car bounds) (cdr bounds))
+      (error "No %s at point" thing))))
+
+(defun kill-word-at-point ()
+  "Kill the word at point."
+  (interactive)
+  (kill-thing-at-point 'word))
+
+(defun convert-to-vw (px-v)
+  (format "%.2f" (/ (float (* (string-to-number px-v) 100)) viewpoint-width)))
+
+(defun convert-to-vh (px-v)
+  (format "%.2f" (/ (float (* (string-to-number px-v) 100)) viewpoint-height )))
+
+(defun px->vw ()
+  "Convert word at point (or selected region) from px to vw."
+  (interactive)
+  (let* ((char (if (use-region-p)
+                     (cons (region-beginning) (region-end))
+                   (thing-at-point 'symbol))))
+    (when (and char (not (string-blank-p char)) )
+      (if (string-match "px" char)
+      (progn
+        (setq word (current-word char))
+        (setq px-v (substring word 0 (- (length word) 2)))
+        (kill-word-at-point)
+        (insert (concat (convert-to-vw px-v) "vw")))
+      (print "there's no px value to converting")))))
+
+;;(global-set-key (kbd "C-c w") 'px->vw)
+;;(global-set-key [f9] 'px->vw)
+(provide 'px2vw)
+;;; px2vw.el ends here
--- px2vw.el
+++ px2vw.el
@@ -1,43 +1,7 @@
-
-(defvar viewpoint-width 1440) 
-(defvar viewpoint-height 900) 
-
-(defun kill-thing-at-point (thing)
-  "Kill the `thing-at-point' for the specified kind of THING."
-  (let ((bounds (bounds-of-thing-at-point thing)))
-    (if bounds
-        (kill-region (car bounds) (cdr bounds))
-      (error "No %s at point" thing))))
-
-(defun kill-word-at-point ()
-  "Kill the word at point."
-  (interactive)
-  (kill-thing-at-point 'word))
-
-(defun convert-to-vw (px-v)
-  (format "%.2f" (/ (float (* (string-to-number px-v) 100)) viewpoint-width)))
-
-(defun convert-to-vh (px-v)
-  (format "%.2f" (/ (float (* (string-to-number px-v) 100)) viewpoint-height )))
-
-(defun px->vw ()
-  "Convert word at point (or selected region) from px to vw."
-  (interactive)
-  (let* ((char (if (use-region-p)
-                   (cons (region-beginning) (region-end))
-                 (thing-at-point 'symbol))))
-    (when (and char (not (string-blank-p char)) )
-      (if (string-match "px" char)
-          (progn
-            (setq word (current-word char))
-            (setq px-v (substring word 0 (- (length word) 2)))
-            (kill-word-at-point)
-            (insert (concat (convert-to-vw px-v) "vw")))
-        (print "there's no px value to converting")))))
-
-;;(global-set-key (kbd "C-c w") 'px->vw)
-;;(global-set-key [f9] 'px->vw)
-
+;;; px2vw.el --- convert px to vw in css
+;;; Commentary:
+;;; Code:
+ 
 (defun region-px->vw (pos1 pos2)
   "当前选中region或者paragraph中的px转化为vw，可能还需要考虑:
    1. 1px的情况
@@ -57,7 +21,12 @@
                         "%.2f"
                         (/
                          (* 100.0 px-digit)
-                         1400.0))))
+                         1440.0))))
         (replace-match (concat vw-digit  "vw") t nil)))))
+ 
+;;(global-set-key (kbd "C-c w") 'px->vw)
+;;(global-set-key [f9] 'px->vw)
 
 (provide 'px2vw)
+
+;;; px2vw.el ends here
